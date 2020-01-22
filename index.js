60e3dba372e70fe592ebee91cd17aa7b81041b67
Samar Omar <samaromm98@gmail.com>
2020-01-22 18:51:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1');
 newHeader.id='victory';
+newHeader.innerHTML='Samar is the champion';
 element.appendChild(newHeader);
