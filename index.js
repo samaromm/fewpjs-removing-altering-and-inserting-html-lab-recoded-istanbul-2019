f23f12fbc779c90981fc246dcc124ade0ea511b1
Samar Omar <samaromm98@gmail.com>
2020-01-22 18:50:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your code here!
 document.querySelector('main#main').remove();
-let forh1 = document.createElement('h1');
-forh1.id='victory';
-let newHeader = document.createElement('header');
-newHeader.appendChild(forh1);
+let newHeader = document.createElement('h1');
+newHeader.id='victory';
+element.appendChild(newHeader);
